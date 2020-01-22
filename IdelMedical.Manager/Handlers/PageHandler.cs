188ad1af056b79d87aedcb8f07592574a67fc9c1
pc <pc@DESKTOP-QGUEP3V>
2020-01-23 03:10:11 +0900

    관리자 - 메인관리 - 목록관리진행중

---
+++ IdelMedical.Manager/Handlers/PageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdelMedical.Manager.Handlers
+{
+    public class PageHandler
+    {
+        public int PrevPage { get; private set; }
+        public int NextPage { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int TotalPage { get; private set; }
+
+        public int TotalItemCount { get; private set; }
+        public int TakeItemCount { get; private set; }
+
+        public PageHandler(int currentPage, int totalItemCount, int takeItemCount)
+        {
+            var totalPage = (int)Math.Ceiling((double)totalItemCount / takeItemCount);
+            totalPage = totalPage < 1 ? 1 : totalPage;
+            currentPage = currentPage < 1 ? 1 : currentPage;
+            currentPage = currentPage > totalPage ? totalPage : currentPage;
+
+            CurrentPage = currentPage;
+            TotalPage = totalPage;
+            PrevPage = currentPage <= 1 ? 1 : currentPage - 1;
+            NextPage = currentPage >= totalPage ? totalPage : currentPage + 1;
+
+            TotalItemCount = totalItemCount;
+            TakeItemCount = takeItemCount;
+        }
+    }
+}
