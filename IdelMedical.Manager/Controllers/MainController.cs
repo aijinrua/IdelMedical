bb9c42cd19556a68e9e2b3a3421794ef8985743b
pc <pc@DESKTOP-QGUEP3V>
2020-01-25 21:16:06 +0900

    게시판먼저 메인화면관리 보류.

--- IdelMedical.Manager/Controllers/MainController.cs
+++ IdelMedical.Manager/Controllers/MainController.cs
@@ -51,5 +51,50 @@ namespace IdelMedical.Manager.Controllers
             ViewBag.SlideData = slideTest;
             return View();
         }
+        public IActionResult Slide(string Search, int Page = 1)
+        {
+            var slideTest = new List<SlideModel>();
+
+
+            int testItemCount = 50;
+
+
+
+            //if (!string.IsNullOrWhiteSpace(Search))
+            //{
+            //    list = list
+            //        .Where(x => x.NickName.IndexOf(Search) >= 0 ||
+            //                    x.UserId.IndexOf(Search) >= 0 ||
+            //                    x.TelegramId.IndexOf(Search) >= 0);
+            //}
+
+
+
+            for (int i = 0; i < testItemCount; i++)
+            {
+                var item = i + 1;
+                slideTest.Add(new SlideModel()
+                {
+                    Number = item,
+                    Title = "SlideTitle_" + (item < 10 ? ("0" + item.ToString()) : item.ToString()),
+                    UploadTime = DateTime.Now.ToString("yyyy-MM-dd")
+                });
+            }
+
+            //var pager = new PageHandler(Page, totalItemCount, 20);
+            var pager = new PageHandler(Page, testItemCount, 15);
+
+            ViewBag.Pager = pager;
+            ViewBag.SlideData = slideTest;
+            return View();
+        }
+        public IActionResult Youtube(string Search, int Page = 1)
+        {
+            return View();
+        }
+        public IActionResult Instagram(string Search, int Page = 1)
+        {
+            return View();
+        }
     }
 }
