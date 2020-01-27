13b2fee1f97dd2f4afbd1f6b38faadbd93514ecb
pc <pc@DESKTOP-QGUEP3V>
2020-01-27 09:38:54 +0900

    .

--- IdelMedical.Manager/Controllers/IdelTVController.cs
+++ IdelMedical.Manager/Controllers/IdelTVController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdelMedical.Manager.DataModels;
+using IdelMedical.Manager.Handlers;
 
 namespace IdelMedical.Manager.Controllers
 {
@@ -11,8 +13,79 @@ namespace IdelMedical.Manager.Controllers
     /// </summary>
     public class IdelTVController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string Search, int Page = 1)
         {
+            var slideTest = new List<SlideModel>();
+
+
+            int testItemCount = 50;
+
+
+
+            //if (!string.IsNullOrWhiteSpace(Search))
+            //{
+            //    list = list
+            //        .Where(x => x.NickName.IndexOf(Search) >= 0 ||
+            //                    x.UserId.IndexOf(Search) >= 0 ||
+            //                    x.TelegramId.IndexOf(Search) >= 0);
+            //}
+
+
+
+            for (int i = 0; i < testItemCount; i++)
+            {
+                var item = i + 1;
+                slideTest.Add(new SlideModel()
+                {
+                    Number = item,
+                    Title = "SlideTitle_" + (item < 10 ? ("0" + item.ToString()) : item.ToString()),
+                    UploadTime = DateTime.Now.ToString("yyyy-MM-dd")
+                });
+            }
+
+            //var pager = new PageHandler(Page, totalItemCount, 20);
+            var pager = new PageHandler(Page, testItemCount, 15);
+
+            ViewBag.Pager = pager;
+            ViewBag.SlideData = slideTest;
+            return View();
+        }
+
+        public IActionResult Detail(string Search, int Page = 1)
+        {
+            var slideTest = new List<SlideModel>();
+
+
+            int testItemCount = 50;
+
+
+
+            //if (!string.IsNullOrWhiteSpace(Search))
+            //{
+            //    list = list
+            //        .Where(x => x.NickName.IndexOf(Search) >= 0 ||
+            //                    x.UserId.IndexOf(Search) >= 0 ||
+            //                    x.TelegramId.IndexOf(Search) >= 0);
+            //}
+
+
+
+            for (int i = 0; i < testItemCount; i++)
+            {
+                var item = i + 1;
+                slideTest.Add(new SlideModel()
+                {
+                    Number = item,
+                    Title = "SlideTitle_" + (item < 10 ? ("0" + item.ToString()) : item.ToString()),
+                    UploadTime = DateTime.Now.ToString("yyyy-MM-dd")
+                });
+            }
+
+            //var pager = new PageHandler(Page, totalItemCount, 20);
+            var pager = new PageHandler(Page, testItemCount, 15);
+
+            ViewBag.Pager = pager;
+            ViewBag.SlideData = slideTest;
             return View();
         }
     }
